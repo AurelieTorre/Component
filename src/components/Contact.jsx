86a3d99232a5185300727e109abb36f939ee8a22
AurelieTorre <aurelie.torre@laposte.net>
2024-06-25 09:48:11 +0200

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,6 @@
+/* Les valeurs du nom, de l'image de profil et du statut connecté (représenté par le rond vert, ou rouge) 
+devront provenir du composant parent App.jsx. 
+-> Tu trouveras des informations sur les props dans la doc en pièce jointe.
+-> Tu trouveras des informations sur l'affichage conditionnel en pièce jointe aussi (qui utilise un opérateur ternaire).
+Dans le composant App, appelle plusieurs fois ton composant Contact, avec des valeurs de props différentes, 
+afin de tester que tout fonctionne correctement. */
