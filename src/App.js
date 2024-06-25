b162a642cfc5773475019f333f05082b5e251023
AurelieTorre <aurelie.torre@laposte.net>
2024-06-25 09:59:18 +0200

    fix hello world

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 
 function App() {
-  function App() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Hello, world!</h1>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, world!</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
